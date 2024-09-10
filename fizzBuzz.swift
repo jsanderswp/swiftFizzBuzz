9d7b24b48c9d301a3650f501bfebf8cd412106bc
jsanderswp <115518944+jsanderswp@users.noreply.github.com>
2024-09-10 16:12:31 -0700

    Update fizzBuzz.swift

--- fizzBuzz.swift
+++ fizzBuzz.swift
@@ -1,1 +1,16 @@
-
+func main() {
+  for i in 1...100 {
+    switch i {
+      case i % 3 == 0:
+        print("Fizz")
+      
+      case i % 5 == 0:
+        print("Buzz")
+      
+      case i % 3 == 0 && i % 5 == 0:
+        print("FizzBuzz")
+  
+      default:
+        print(i)
+    }
+    
